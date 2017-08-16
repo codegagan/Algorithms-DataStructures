5c359850ff795426f7b1051b40aff91af9ec44d6
Prafful Agarwal <prafful.agrwal@gmail.com>
2017-08-16 13:21:38 +0530

    Create Dijkstra.java

---
+++ Graphs/Dijkstra/Dikkstra2/Dijkstra.java
@@ -0,0 +1,120 @@
+import java.util.PriorityQueue;
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.io.*;
+
+class Vertex implements Comparable<Vertex>
+{
+    public final String name;
+    public Edge[] adjacencies;
+    public double minDistance = Double.POSITIVE_INFINITY;
+    public Vertex previous;
+    public Vertex(String argName) { name = argName; }
+    public String toString() { return name; }
+    public int compareTo(Vertex other)
+    {
+        return Double.compare(minDistance, other.minDistance);
+    }
+}
+
+class Edge
+{
+    public final Vertex target;
+    public final double weight;
+    public Edge(Vertex argTarget, double argWeight)
+    { target = argTarget; weight = argWeight; }
+}
+
+public class Dijkstra
+{
+    public static void computePaths(Vertex source)
+    {
+        source.minDistance = 0.;
+        PriorityQueue<Vertex> vertexQueue = new PriorityQueue<Vertex>();
+      	vertexQueue.add(source);
+
+	while (!vertexQueue.isEmpty()) {
+	    Vertex u = vertexQueue.poll();
+
+            
+            for (Edge e : u.adjacencies)
+            {
+                Vertex v = e.target;
+                double weight = e.weight;
+                double distanceThroughU = u.minDistance + weight;
+		if (distanceThroughU < v.minDistance) {
+		    vertexQueue.remove(v);
+		    v.minDistance = distanceThroughU ;
+		    v.previous = u;
+		    vertexQueue.add(v);
+		}
+            }
+        }
+    }
+
+    public static List<Vertex> getShortestPathTo(Vertex target)
+    {
+        List<Vertex> path = new ArrayList<Vertex>();
+        for (Vertex vertex = target; vertex != null; vertex = vertex.previous)
+            path.add(vertex);
+        Collections.reverse(path);
+        return path;
+    }
+
+    public static void main(String[] args)throws Exception
+    {
+        Vertex v0 = new Vertex("A");
+	Vertex v1 = new Vertex("B");
+	Vertex v2 = new Vertex("C");
+	Vertex v3 = new Vertex("D");
+	Vertex v4 = new Vertex("E");
+
+	v0.adjacencies = new Edge[]{ new Edge(v1, 5),
+	                             new Edge(v2, 10),
+                               new Edge(v3, 8) };
+	v1.adjacencies = new Edge[]{ new Edge(v0, 5),
+	                             new Edge(v2, 3),
+	                             new Edge(v4, 7) };
+	v2.adjacencies = new Edge[]{ new Edge(v0, 10),
+                               new Edge(v1, 3) };
+	v3.adjacencies = new Edge[]{ new Edge(v0, 8),
+	                             new Edge(v4, 2) };
+	v4.adjacencies = new Edge[]{ new Edge(v1, 7),
+                               new Edge(v3, 2) };
+	Vertex[] vertices = { v0, v1, v2, v3, v4 };
+	System.out.println("Enter the Starting Point");
+	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	String s = br.readLine();
+
+	if(s.equals("A"))
+        	computePaths(v0);
+	
+	else if(s.equals("B"))
+        	computePaths(v1);
+
+	else if(s.equals("C"))
+        	computePaths(v2);
+	
+	else if(s.equals("D"))
+        	computePaths(v3);
+
+	else if(s.equals("E"))
+        	computePaths(v4);
+	
+	System.out.println("Enter the ending point");
+	
+	String e = br.readLine();
+
+	
+        for (Vertex v : vertices)
+	{
+	if(e.equals(v.toString()))
+	{
+	    System.out.println("Distance to " + v + ": " + v.minDistance);
+	    List<Vertex> path = getShortestPathTo(v);
+	    System.out.println("Path: " + path);
+	}
+	}
+    }
+}
